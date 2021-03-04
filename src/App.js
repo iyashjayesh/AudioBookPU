9d609a6c5adf09c30a326bf3549806db8dd05aed
iyashjayesh <180303105046@paruluniversity.ac.in>
2021-03-04 12:21:10 +0530

    Footer Added and chages made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './globalComponents/Header/Header';
+import Footer from './globalComponents/Footer/Footer';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import home from './MainPages/Home';
 import c1 from './MainPages/c1';
@@ -16,16 +17,17 @@ function App() {
     <BrowserRouter>
       <div className="App">
           <Header/>
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route path="/chapters" component={Chapters}/>
-            <Route path="/chapter-one" component={c1} />
-            <Route path="/chapter-two" component={c2} />
-            <Route path="/chapter-three" component={c3} />
-            <Route path="/chapter-four" component={c4} />
-            <Route path="/chapter-five" component={c5} />
-            <Route path="/chapter-six" component={c6} />
-          </Switch>
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route path="/chapters" component={Chapters}/>
+              <Route path="/chapter-one" component={c1} />
+              <Route path="/chapter-two" component={c2} />
+              <Route path="/chapter-three" component={c3} />
+              <Route path="/chapter-four" component={c4} />
+              <Route path="/chapter-five" component={c5} />
+              <Route path="/chapter-six" component={c6} />
+            </Switch>
+          <Footer/>
       </div>
     </BrowserRouter>
   );

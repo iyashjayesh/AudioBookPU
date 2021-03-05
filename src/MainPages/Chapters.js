8e4240305f2d80c259e2f6ca10ab8208e99770e9
iyashjayesh <180303105046@paruluniversity.ac.in>
2021-03-05 18:20:27 +0530

    img added

--- src/MainPages/Chapters.js
+++ src/MainPages/Chapters.js
@@ -2,6 +2,12 @@ import React from 'react'
 import '../App.css'
 import Chaptercard from '../globalComponents/ChapterCard/Chaptercard'
 import ch1 from '../img/ch/ch1.png';
+import ch2 from '../img/ch/ch2.png';
+import ch3 from '../img/ch/ch3.png';
+import ch4 from '../img/ch/ch4.png';
+import ch5 from '../img/ch/ch5.png';
+import ch6 from '../img/ch/ch6.png';
+
 
 const Chapters = () => {
     return (
@@ -9,8 +15,8 @@ const Chapters = () => {
             <div className="conatiner text-center mt-3">
                 <h1><strong>List of Chapters | अध्याय</strong></h1>
             </div>
-            <Chaptercard img1={ch1} path1="/chapter-one" img2={ch1} path2="/chapter-two" img3={ch1} path3="/chapter-three"/>
-            <Chaptercard img1={ch1} path1="/chapter-four" img2={ch1} path2="/chapter-five" img3={ch1} path3="chapter-six"/>
+            <Chaptercard img1={ch1} path1="/chapter-one" img2={ch2} path2="/chapter-two" img3={ch3} path3="/chapter-three"/>
+            <Chaptercard img1={ch4} path1="/chapter-four" img2={ch5} path2="/chapter-five" img3={ch6} path3="chapter-six"/>
         </div>
     )
 }
--- src/globalComponents/Footer/Footer.js
+++ src/globalComponents/Footer/Footer.js
@@ -1,115 +1,115 @@
-import { FaGithub, FaLinkedinIn, FaFacebookSquare } from "react-icons/fa";
-import React from 'react';
-import '../Footer/footer.scss';
+// import { FaGithub, FaLinkedinIn, FaFacebookSquare } from "react-icons/fa";
+// import React from 'react';
+// import '../Footer/footer.scss';
 
-const Footer = () => {
-    return (
-        <footer>
-            <div class="container">
-                <div class="row">
-                <div class="col-md-4 footer-column">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <span class="footer-title">List of Chapters</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 1</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 2</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 3</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 4</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 5</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chapter 6</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4 footer-column">
-                    <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <span class="footer-title">AudioBookPU</span>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">XYZ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">XYZ</a>
-                    </li>
-                    </ul>
-                </div>
-                <div class="col-md-4 footer-column">
-                    <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <span class="footer-title">Contact & Support</span>
-                    </li>
-                    <li class="nav-item">
-                        <span class="nav-link"><i class="fas fa-phone"></i>+91 9925877996</span>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fas fa-comments"></i>Live chat</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fas fa-envelope"></i>Contact us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fas fa-star"></i>Give feedback</a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
+// const Footer = () => {
+//     return (
+//         <footer>
+//             <div class="container">
+//                 <div class="row">
+//                 <div class="col-md-4 footer-column">
+//                     <ul class="nav flex-column">
+//                         <li class="nav-item">
+//                             <span class="footer-title">List of Chapters</span>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 1</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 2</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 3</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 4</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 5</a>
+//                         </li>
+//                         <li class="nav-item">
+//                             <a class="nav-link" href="#">Chapter 6</a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//                 <div class="col-md-4 footer-column">
+//                     <ul class="nav flex-column">
+//                     <li class="nav-item">
+//                         <span class="footer-title">AudioBookPU</span>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#">About us</a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#">XYZ</a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#">XYZ</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 <div class="col-md-4 footer-column">
+//                     <ul class="nav flex-column">
+//                     <li class="nav-item">
+//                         <span class="footer-title">Contact & Support</span>
+//                     </li>
+//                     <li class="nav-item">
+//                         <span class="nav-link"><i class="fas fa-phone"></i>+91 9925877996</span>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#"><i class="fas fa-comments"></i>Live chat</a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#"><i class="fas fa-envelope"></i>Contact us</a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#"><i class="fas fa-star"></i>Give feedback</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 </div>
 
-                <div class="text-center"><i class="fas fa-ellipsis-h"></i></div>
+//                 <div class="text-center"><i class="fas fa-ellipsis-h"></i></div>
                 
-                <div class="row text-center">
-                <div class="col-md-4 box">
-                    <span class="copyright quick-links">Copyright &copy; AudioBookPU <script>document.write(new Date().getFullYear())</script>
-                    </span>
-                </div>
-                <div class="col-md-4 box">
-                    <ul class="list-inline social-buttons">
-                    <li class="list-inline-item">
-                        <a href="#">
-                            <FaFacebookSquare/>
-                            {/* <i class="fab fa-facebook-f"></i> */}
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                            <FaLinkedinIn/>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                            <FaGithub/>
-                        </a>
-                    </li>
-                    </ul>
-                </div>
-                <div class="col-md-4 box">
-                    <ul class="list-inline quick-links">
-                    <li class="list-inline-item">
-                        <a href="#">Privacy Policy</a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">Terms of Use</a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </footer>
-    )
-}
+//                 <div class="row text-center">
+//                 <div class="col-md-4 box">
+//                     <span class="copyright quick-links">Copyright &copy; AudioBookPU <script>document.write(new Date().getFullYear())</script>
+//                     </span>
+//                 </div>
+//                 <div class="col-md-4 box">
+//                     <ul class="list-inline social-buttons">
+//                     <li class="list-inline-item">
+//                         <a href="#">
+//                             <FaFacebookSquare/>
+//                             {/* <i class="fab fa-facebook-f"></i> */}
+//                         </a>
+//                     </li>
+//                     <li class="list-inline-item">
+//                         <a href="#">
+//                             <FaLinkedinIn/>
+//                         </a>
+//                     </li>
+//                     <li class="list-inline-item">
+//                         <a href="#">
+//                             <FaGithub/>
+//                         </a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 <div class="col-md-4 box">
+//                     <ul class="list-inline quick-links">
+//                     <li class="list-inline-item">
+//                         <a href="#">Privacy Policy</a>
+//                     </li>
+//                     <li class="list-inline-item">
+//                         <a href="#">Terms of Use</a>
+//                     </li>
+//                     </ul>
+//                 </div>
+//                 </div>
+//             </div>
+//         </footer>
+//     )
+// }
 
-export default Footer
+// export default Footer
